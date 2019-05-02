46f55d7047b0d51793120cd5a6461a7cc32c0a99
Aaron Altounian <aaron.altounian@gmail.com>
2019-05-02 14:19:01 -0500

    started, GET works for users & user by id

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 8080;
 
+const state = require('./state');
+let users = state.users;
+let products = state.products;
+
+let newUser = {
+    'name': 'Bender Rodriguez',
+}
+
+app.get('/users', (req, res) => {
+    res.json(state.users);
+})
+
+app.get('/users/:user', (req, res) => {
+    let id = req.params.user;
+    let user = users.find( u => u['id'] === Number(id) );
+    res.json(user)
+})
+
+app.post('/users', (req, res) => {
+    
+})
+
+app.listen( port, () => console.log(`Example app listening on port ${port}!`) )
