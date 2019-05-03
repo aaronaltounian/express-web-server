41381a5ba6f9c22f1b9047cda7a03b1fff1b9c82
Aaron Altounian <aaron.altounian@gmail.com>
2019-05-02 20:17:24 -0500

    started trying to get the methods achieved using router. get works but can't figure out the rest yet

--- Controllers/user-controllers.js
+++ Controllers/user-controllers.js
@@ -1,36 +1,22 @@
-const express = require('express');
-const app = express();
-const port = 8080;
+const state = require('../state');
+const users = state.users;
 
-const state = require('./state');
-let users = state.users;
-
-// const userRoutes = require('./user-routes');
-
-// app.use('/', userRoutes);
-
-const bodyParser = require('body-parser');
-// let jsonParser = bodyParser.json();
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-// get all users:
-app.get('/users', (req, res) => {
+exports.list = (req, res) => {
     res.json(state.users);
-})
+}
 
 // get specific user by ID:
-app.get('/users/:userId', (req, res) => {
+exports.show = (req, res) => {
     // store the path after users/ in a variable for comparison:
     let id = req.params.userId;
     // find the user whose ID matches the path:
     let user = users.find( u => u['id'] === Number(id) );
     // send the user back to the client:
     res.json(user)
-})
+}
 
 // post a new user using body-parser to get data from the client:
-app.post('/users', (req, res, next) => {
+exports.post = (req, res, next) => {
     // create a variable newUser from the parsed JSON in the body:
     let newUser = req.body;
     // create an id for the new user based on the existing ids:
@@ -39,10 +25,10 @@ app.post('/users', (req, res, next) => {
     users.push(newUser);
     // send the newUser to the client:
     res.json(newUser);
-})
+}
 
 // update a user, using body-parser to get data from the client:
-app.put('/users/:userId', (req, res) => {
+exports.update = (req, res) => {
     // create updated variable to store the parsed JSON from the body:
     let updated = req.body;
     // create id variable to store the path:
@@ -53,10 +39,10 @@ app.put('/users/:userId', (req, res) => {
     user.occupation = updated.occupation;
     // send the updated user's info back to the client:
     res.json(user);
-})
+}
 
 // delete a user:
-app.delete('/users/:userId', (req, res) => {
+exports.remove = (req, res) => {
     // create id variable to store the path:
     let id = Number(req.params.userId);
     // find the user whose ID matches the path:
@@ -65,6 +51,4 @@ app.delete('/users/:userId', (req, res) => {
     user.isActive = false;
     // send a message back to the client about which user has been deleted:
     res.send( `Deleted user #${id}: ${user.name} \n ${JSON.stringify(user)}` );
-})
-
-app.listen( port, () => console.log(`Example app listening on port ${port}!`) )
+}
---
+++ user-routes.js
@@ -0,0 +1,73 @@
+const express = require('express');
+let app = express();
+const bodyParser = require('body-parser');
+const state = require('./state');
+let users = state.users;
+let router = express.Router();
+
+const { list, show, post, update, remove } = require('./Controllers/user-controllers')
+// let jsonParser = bodyParser.json();
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+router.get('/users', list);
+router.get('/users/:userId', show);
+router.post('/users', post);
+router.put('/users/:userId', update);
+router.delete('/users/:userId', remove);
+
+
+// // get all users:
+// app.get('/users', (req, res) => {
+//     res.json(state.users);
+// })
+
+// // get specific user by ID:
+// app.get('/users/:userId', (req, res) => {
+//     // store the path after users/ in a variable for comparison:
+//     let id = req.params.userId;
+//     // find the user whose ID matches the path:
+//     let user = users.find( u => u['id'] === Number(id) );
+//     // send the user back to the client:
+//     res.json(user)
+// })
+
+// // post a new user using body-parser to get data from the client:
+// app.post('/users', (req, res, next) => {
+//     // create a variable newUser from the parsed JSON in the body:
+//     let newUser = req.body;
+//     // create an id for the new user based on the existing ids:
+//     newUser.id = users[users.length - 1].id + 1;
+//     // push the newUser into the users array:
+//     users.push(newUser);
+//     // send the newUser to the client:
+//     res.json(newUser);
+// })
+
+// // update a user, using body-parser to get data from the client:
+// app.put('/users/:userId', (req, res) => {
+//     // create updated variable to store the parsed JSON from the body:
+//     let updated = req.body;
+//     // create id variable to store the path:
+//     let id = req.params.userId;
+//     // find the user whose ID matches the path:
+//     let user = users.find( u => u['id'] === Number(id) );
+//     // set the user's occupation to the updated occupation:
+//     user.occupation = updated.occupation;
+//     // send the updated user's info back to the client:
+//     res.json(user);
+// })
+
+// // delete a user:
+// app.delete('/users/:userId', (req, res) => {
+//     // create id variable to store the path:
+//     let id = Number(req.params.userId);
+//     // find the user whose ID matches the path:
+//     let user = users.find( u => u['id'] === id );
+//     // instead of deleting user, set inactive:
+//     user.isActive = false;
+//     // send a message back to the client about which user has been deleted:
+//     res.send( `Deleted user #${id}: ${user.name} \n ${JSON.stringify(user)}` );
+// })
+
+module.exports = router;
